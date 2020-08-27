6602754e75acf56a35b71a16bf2570ae855cfe9e
Amin <valiamin21@gmail.com>
2020-08-27 22:53:31 +0430

    making splash screen

--- app/src/main/java/ir/proglovving/fitapp/SplashActivity.java
+++ app/src/main/java/ir/proglovving/fitapp/SplashActivity.java
@@ -3,11 +3,13 @@ package ir.proglovving.fitapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

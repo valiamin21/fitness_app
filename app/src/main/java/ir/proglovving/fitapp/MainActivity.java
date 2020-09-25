4131a6eef90bd087c0aa980de86b351be79b4996
Amin <valiamin21@gmail.com>
2020-09-25 10:23:47 +0330

    renaming SplashActivity to MainActivity

--- app/src/main/java/ir/proglovving/fitapp/MainActivity.java
+++ app/src/main/java/ir/proglovving/fitapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class SplashActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

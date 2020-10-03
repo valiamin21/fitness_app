5b7294e861d763c17aa53f1666bb32a3b6a6c88e
Amin <valiamin21@gmail.com>
2020-10-03 22:53:11 +0330

    adding getCategory() method to ApiService

--- app/src/main/java/ir/proglovving/fitapp/api/ApiService.java
+++ app/src/main/java/ir/proglovving/fitapp/api/ApiService.java
@@ -1,10 +1,15 @@
 package ir.proglovving.fitapp.api;
 
 import ir.proglovving.fitapp.data_models.CategoriesPack;
+import ir.proglovving.fitapp.data_models.Category;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface ApiService {
     @GET("categories")
     Call<CategoriesPack> getCategories();
+
+    @GET("category/{id}")
+    Call<Category> getCategory(@Path("id") int categoryId);
 }

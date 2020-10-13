1cc6d13ae10b96edd9061e94817e0862badee03c
Amin <valiamin21@gmail.com>
2020-10-13 11:27:48 +0330

    adding getDay() method to ApiService

--- app/src/main/java/ir/proglovving/fitapp/api/ApiService.java
+++ app/src/main/java/ir/proglovving/fitapp/api/ApiService.java
@@ -1,8 +1,10 @@
 package ir.proglovving.fitapp.api;
 
+import io.reactivex.Observable;
 import io.reactivex.Single;
 import ir.proglovving.fitapp.data_models.CategoriesPack;
 import ir.proglovving.fitapp.data_models.Category;
+import ir.proglovving.fitapp.data_models.Day;
 import ir.proglovving.fitapp.data_models.Plan;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -16,4 +18,7 @@ public interface ApiService {
 
     @GET("plan/{id}")
     Single<Plan> getPlan(@Path("id") int planId);
+
+    @GET("day/{id}")
+    Observable<Day> getDay(@Path("id") int dayId);
 }

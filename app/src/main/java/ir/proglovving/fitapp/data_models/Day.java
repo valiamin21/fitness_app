00afc5d4da9cdc43fad61789b6ea2503a0ffece1
Amin <valiamin21@gmail.com>
2020-10-13 10:53:26 +0330

    modifying Day data model

--- app/src/main/java/ir/proglovving/fitapp/data_models/Day.java
+++ app/src/main/java/ir/proglovving/fitapp/data_models/Day.java
@@ -4,18 +4,17 @@ import com.google.gson.annotations.SerializedName;
 
 public class Day {
     private int id;
+    @SerializedName("name")
     private String title;
     private String muscle;
     @SerializedName("exercise_id")
     private int[] exercisesIds;
-    private int exercisesCount;
-    private int doneExercisesCount;
 
-    public int getId(){
+    public int getId() {
         return id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
@@ -27,39 +26,19 @@ public class Day {
         this.title = title;
     }
 
-    public void setMuscle(String muscle){
+    public void setMuscle(String muscle) {
         this.muscle = muscle;
     }
 
-    public String getMuscle(){
+    public String getMuscle() {
         return muscle;
     }
 
-    public void setExercisesIds(int[] exercisesIds){
+    public void setExercisesIds(int[] exercisesIds) {
         this.exercisesIds = exercisesIds;
     }
 
-    public int[] getExercisesIds(){
+    public int[] getExercisesIds() {
         return exercisesIds;
     }
-
-   public int getExercisesCount(){
-        return exercisesCount;
-   }
-
-   public void setExercisesCount(int exercisesCount){
-        this.exercisesCount = exercisesCount;
-   }
-
-   public int getDoneExercisesCount(){
-        return doneExercisesCount;
-   }
-
-   public void setDoneExercisesCount(int doneExercisesCount){
-        this.doneExercisesCount = doneExercisesCount;
-   }
-
-   public int getDonePercent(){
-        return doneExercisesCount * 100 / exercisesCount;
-   }
 }

eac9f99db0aa8420bcc63de761300c82e99c2ba4
Amin <valiamin21@gmail.com>
2020-10-13 11:35:41 +0330

    loading Day items in DayItemsActivity

--- app/src/main/java/ir/proglovving/fitapp/viesws/DayItemsActivity.java
+++ app/src/main/java/ir/proglovving/fitapp/viesws/DayItemsActivity.java
@@ -2,6 +2,7 @@ package ir.proglovving.fitapp.viesws;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -9,16 +10,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.util.List;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.Single;
 import io.reactivex.SingleObserver;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
 import ir.proglovving.cfviews.CTypefaceProvider;
 import ir.proglovving.fitapp.R;
+import ir.proglovving.fitapp.adapters.DayItemsRecyclerAdapter;
 import ir.proglovving.fitapp.api.ApiService;
 import ir.proglovving.fitapp.api.RetrofitClient;
+import ir.proglovving.fitapp.data_models.Day;
 import ir.proglovving.fitapp.data_models.Plan;
 
 public class DayItemsActivity extends AppCompatActivity {
@@ -27,6 +38,7 @@ public class DayItemsActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private RecyclerView dayItemsRecyclerView;
+    private DayItemsRecyclerAdapter dayItemsRecyclerAdapter;
 
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
@@ -54,7 +66,7 @@ public class DayItemsActivity extends AppCompatActivity {
 
                     @Override
                     public void onSuccess(Plan plan) {
-                        toolbar.setTitle(plan.getTitle());
+                        applyPlan(plan);
                     }
 
                     @Override
@@ -64,10 +76,54 @@ public class DayItemsActivity extends AppCompatActivity {
                 });
     }
 
+    private void applyPlan(Plan plan) {
+        toolbar.setTitle(plan.getTitle());
+        getDaysObservable(RetrofitClient.getApiService(), plan.getDaysIds())
+                .subscribe(new Observer<Day>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        compositeDisposable.add(d);
+                    }
+
+                    @Override
+                    public void onNext(Day day) {
+                        dayItemsRecyclerAdapter.addItem(day);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        // TODO: 10/13/20
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        // TODO: 10/13/20
+                    }
+                });
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         compositeDisposable.dispose();
+    }
+
+    private Observable<Day> getDaysObservable(final ApiService apiService, final int[] days) {
+        return Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                for (int day : days) {
+                    if (!emitter.isDisposed()) {
+                        emitter.onNext(day);
+                    }
+                }
+            }
+        }).concatMap(new Function<Integer, ObservableSource<? extends Day>>() {
+            @Override
+            public ObservableSource<? extends Day> apply(Integer integer) throws Exception {
+                return apiService.getDay(integer);
+            }
+        }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 
     private void initViews() {
@@ -79,5 +135,7 @@ public class DayItemsActivity extends AppCompatActivity {
             }
         }, 10);
         dayItemsRecyclerView = findViewById(R.id.day_items_list_recyclerView);
+        dayItemsRecyclerAdapter = new DayItemsRecyclerAdapter(this);
+        dayItemsRecyclerView.setAdapter(dayItemsRecyclerAdapter);
     }
 }

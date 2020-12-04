caf3c189a9207ce75f75c2d76da05924414dae28
Amin <valiamin21@gmail.com>
2020-12-04 13:02:45 +0330

    showing day number in day items

--- app/src/main/java/ir/proglovving/fitapp/Utilities.java
+++ app/src/main/java/ir/proglovving/fitapp/Utilities.java
@@ -27,4 +27,18 @@ public class Utilities {
         mNewTitle.setSpan(new CustomTypefaceSpan("", typeface), 0, mNewTitle.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         mi.setTitle(mNewTitle);
     }
+
+    public static String englishToPersianNumber(int number){
+        return String.valueOf(number)
+                .replace('0','۰')
+                .replace('1','۱')
+                .replace('2','۲')
+                .replace('3','۳')
+                .replace('4','۴')
+                .replace('5','۵')
+                .replace('6','۶')
+                .replace('7','۷')
+                .replace('8','۸')
+                .replace('9','۹');
+    }
 }
--- app/src/main/java/ir/proglovving/fitapp/adapters/DayItemsRecyclerAdapter.java
+++ app/src/main/java/ir/proglovving/fitapp/adapters/DayItemsRecyclerAdapter.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import ir.proglovving.fitapp.R;
+import ir.proglovving.fitapp.Utilities;
 import ir.proglovving.fitapp.data_models.Day;
 import ir.proglovving.fitapp.viesws.DayActivity;
 
@@ -33,8 +34,8 @@ public class DayItemsRecyclerAdapter extends RecyclerView.Adapter<DayItemsRecycl
 
     @Override
     public void onBindViewHolder(@NonNull DayItemViewHolder holder, final int position) {
-        holder.progressTextView.setText(String.valueOf(dayItemList.get(position).getId())); // todo put done exercises percent instead of id
-        holder.titleTextView.setText(dayItemList.get(position).getTitle());
+        holder.progressTextView.setText(Utilities.englishToPersianNumber(position * 2) + "%"); // todo put done exercises percent instead of position
+        holder.titleTextView.setText(context.getString(R.string.day) + " " + Utilities.englishToPersianNumber(position + 1));
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
